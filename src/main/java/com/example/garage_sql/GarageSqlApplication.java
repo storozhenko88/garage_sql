c0e0daf9beba3249a71d0d81ce2faf9d6b527bbe
storozhenko <storozhenko-88@ukr.net>
2022-12-25 15:16:16 +0300

    Initial commit

---
+++ src/main/java/com/example/garage_sql/GarageSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.garage_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/garage_sql/GarageSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garage_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
